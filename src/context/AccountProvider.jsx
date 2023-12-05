1b6db8622797ed58009c52fc75ec7f3926595a5c
Ashfaq Haneef <78129119+mrthangu@users.noreply.github.com>
2023-12-05 14:13:39 +0530

    Update AccountProvider.jsx

--- src/context/AccountProvider.jsx
+++ src/context/AccountProvider.jsx
@@ -12,7 +12,7 @@ export const AccountProvider = ({ children }) => {
 
   const socket = useRef();
   useEffect(() => {
-    socket.current = io("13.127.139.59");
+    socket.current = io("https://whatsapp-socket.mrzera.xyz");
   }, []);
 
   return (
